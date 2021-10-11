8d363ec14e89bc0532b723995d06152fb266fb29
Ashish <ashishkumarvish.432@gmail.com>
2021-10-11 18:40:34 +0530

    Welcome to tictactoe game

--- src/com/bridgelab/tictoetoe/TicTacToeGame.java
+++ src/com/bridgelab/tictoetoe/TicTacToeGame.java
@@ -1,5 +1,24 @@
 package com.bridgelab.tictoetoe;
 
 public class TicTacToeGame {
-    
+    static char[] bord = new char[10]; // bord size 10
+
+    public static void main(String[] args) {
+        createBoard(); // calling method
+        System.out.println("Welcome to Tic Tac Toe Game");
+
+    }
+     /*
+        why this method use:
+        in method data is character
+        And using for loop to iteration from 1 to 9 by ignoring 0
+      */
+     public static void createBoard() {
+         for (int index = 1; index < bord.length; index++){
+             bord[index] = ' ';
+         }
+     }
+
+
+
 }
